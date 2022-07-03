bfb375c42b978f421f7d1689fdd6cb6c83c5ef00
kimcno3 <kimcno3@gmail.com>
2022-07-04 00:18:02 +0900

    Google CheckStyle 설정
    * Config 패키지 내 xml 파일 추가
    * CheckStyle 에 맞게 FootballApplication 양식 수정

--- src/main/java/com/flab/football/FootballApplication.java
+++ src/main/java/com/flab/football/FootballApplication.java
@@ -3,11 +3,13 @@ package com.flab.football;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 어플리케이션 실행 클래스.
+ */
+
 @SpringBootApplication
 public class FootballApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(FootballApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(FootballApplication.class, args);
+  }
 }
