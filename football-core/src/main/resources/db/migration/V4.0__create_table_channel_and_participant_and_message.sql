67f0147f737fa773d95e2a98a2288a80c6b3586f
kimcno3 <kimcno3@gmail.com>
2022-08-24 19:43:08 +0900

    모듈별 build.gradle 추가 및 DB Migration 파일 커밋

---
+++ football-core/src/main/resources/db/migration/V4.0__create_table_channel_and_participant_and_message.sql
@@ -0,0 +1,23 @@
+CREATE TABLE `channel` (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE `message` (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  type VARCHAR(255),
+  content VARCHAR(255),
+  create_at DATETIME,
+  user_id INT,
+  channel_id INT,
+  FOREIGN KEY (user_id) REFERENCES `user` (id),
+  FOREIGN KEY (channel_id) REFERENCES `channel` (id)
+);
+
+CREATE TABLE `participant` (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  channel_id INT,
+  user_id INT,
+  FOREIGN KEY (channel_id) REFERENCES `channel` (id),
+  FOREIGN KEY (user_id) REFERENCES `user` (id)
+);
