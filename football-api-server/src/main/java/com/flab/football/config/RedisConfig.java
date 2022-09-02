02ebba447f9e77fae85beca8e77e8d6031529928
kimcno3 <kimcno3@gmail.com>
2022-09-03 05:57:54 +0900

    AWS 배포를 위한 RDS, Elasticcache 구성 변경
    * application-prod.yml datasource, redis 변수 수정
    * Mysql replication 구성 제거
    * DataSourceConfig.java 삭제
      - JPA는 별도의 config 클래스파일이 필요 없다.
    * redis nodes 지정 코드 제거

--- football-api-server/src/main/java/com/flab/football/config/RedisConfig.java
+++ football-api-server/src/main/java/com/flab/football/config/RedisConfig.java
@@ -1,15 +1,11 @@
 package com.flab.football.config;
 
-import io.lettuce.core.ReadFrom;
-import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -21,11 +17,14 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Getter
 @Setter
 @Configuration
-@ConfigurationProperties(prefix = "spring.redis.cluster")
 public class RedisConfig {
 
-  private List<String> nodes;
 
+  @Value("${spring.redis.host}")
+  private String redisHost;
+
+  @Value("${spring.redis.port}")
+  private int redisPort;
   /**
    * Redis Client 는 Lettuce로 지정합니다.
    * Jedis 보다 Lettuce가 성능적인 부분에서 우수하며, 이로 인해 Jedis는 Spring boot에서 Deprecated 처리 되었습니다.
@@ -34,13 +33,7 @@ public class RedisConfig {
   @Bean
   public RedisConnectionFactory redisConnectionFactory() {
 
-    LettuceClientConfiguration clientConfiguration = LettuceClientConfiguration.builder()
-        .readFrom(ReadFrom.REPLICA_PREFERRED) // Slave 노드에 우선으로 접근
-        .build();
-
-    RedisClusterConfiguration redisClusterConfig = new RedisClusterConfiguration(nodes);
-
-    return new LettuceConnectionFactory(redisClusterConfig, clientConfiguration);
+    return new LettuceConnectionFactory(redisHost, redisPort);
 
   }
 
